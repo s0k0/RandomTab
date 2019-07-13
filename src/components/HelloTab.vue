42e5835f8ca104c1db4d97d44f4912b7dc4c8644
Sophie Kolbe <sophie.kolbe@daimler.com>
2019-07-13 08:54:14 +0200

    basic styling and content toggling

--- src/components/HelloTab.vue
+++ src/components/HelloTab.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div class="tabs-container">
     <div class="tabs">
         <ul>
-          <li v-for="tab in tabs" v-bind:key="tab" :class="{ 'is-active': true}">
-              <a @click="selectTab(tab)">{{ tab }}</a>
+          <li v-for="tab in tabs" v-bind:key="tab.id" :class="{ 'is-active': tab.isActive}">
+              <a @click="selectTab(tab)">{{ tab.name }}</a>
           </li>
         </ul>
       </div>
-
+      <hr>
       <div class="tabs-details">
-        
+         {{content}}
       </div>
   </div>
 </template>
@@ -19,24 +19,52 @@ export default {
   name: 'HelloTab',
   data() {
         return { 
-          tabs: ['one','two','three'] 
+          tabs: [
+            { name: 'Topics', id: '1', isActive: false},
+            { name: 'Archive', id: '2',  isActive: false},
+            { name: 'Pages', id: '3',  isActive: false}
+            ] ,
+          content: 'Hello Conent'  
           };
     },
+    computed: {
+
+    },
+    methods: {
+      selectTab(selectedTab) {
+          this.tabs.forEach(tab => {
+              tab.isActive = (tab.name == selectedTab.name);
+          });
+          this.content = selectedTab.name
+      }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped  lang="scss">
+.tabs-container {
+  padding: 12px;
+  background: rgba(66,185,131,0.1);
+  text-align: left
+}
 ul {
   list-style-type: none;
   padding: 0;
+  margin: 0
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin: 12px 12px 0 0;
+  font-size:larger
 }
 a {
-  color: #42b983;
+  color: grey;
   cursor: pointer;
+}
+.is-active {
+  a {
+    color: #42b983;
+  }
 }
 </style>
